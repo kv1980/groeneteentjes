59c9da80f1ba4b6b1c9a692ea168d38325fe6fbf
Kristof <Kristof@192.168.0.128>
2018-08-22 20:34:27 +0200

    test

---
+++ src/main/java/be/vdab/groenetenen/web/Webconfiguratie.java
@@ -0,0 +1,43 @@
+package be.vdab.groenetenen.web;
+
+import java.util.Locale;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.FixedLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+@Configuration
+class Webconfiguratie implements WebMvcConfigurer {			
+//	@Bean
+//	FixedLocaleResolver localeResolver()	{
+//		return new FixedLocaleResolver(new Locale("fr","BE"));	
+//	}
+
+	
+//	@Bean
+//	SessionLocaleResolver localeResolver() {
+//		return new SessionLocaleResolver();
+//	}
+//	
+//	@Override
+//	public void addInterceptors(InterceptorRegistry registry) {
+//		registry.addInterceptor(new LocaleChangeInterceptor());
+//	}
+	
+	@Bean
+	CookieLocaleResolver localeResolver() {
+		CookieLocaleResolver resolver = new CookieLocaleResolver();
+		resolver.setCookieMaxAge(604_800);
+		return resolver;
+	}
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new LocaleChangeInterceptor());
+	}
+}
